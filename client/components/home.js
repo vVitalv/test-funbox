8863923e7236da062392302126cfd2b23a82e22c
vVitalv <vitalvital@bk.ru>
2022-07-04 22:44:57 +0300

    sass-loader fix

--- client/components/home.js
+++ client/components/home.js
@@ -3,15 +3,12 @@ import { Outlet, useLocation } from 'react-router-dom'
 
 import Head from './head'
 import Sidebar from './sidebar/sidebar'
-import bg from '../assets/images/fone.png'
+// import bg from '../assets/images/fone.png'
 
 const Home = () => {
   const { pathname } = useLocation()
   return (
-    <div
-      className="relative min-h-screen min-w-[375px] bg-repeat"
-      style={{ backgroundImage: `url(${bg})` }}
-    >
+    <div className="relative min-h-screen min-w-[375px] bg-[url(../images/fone.png)] bg-repeat">
       <Head title={pathname} />
       <Sidebar />
       <Outlet />
